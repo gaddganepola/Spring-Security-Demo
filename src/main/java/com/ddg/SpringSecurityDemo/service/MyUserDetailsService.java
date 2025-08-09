21d34f00491799a7fb6406bc297ea0c489740ab5
gaddganepola <ganeypola@gmail.com>
2025-08-09 19:25:59 +0530

    develop user principal model

--- src/main/java/com/ddg/SpringSecurityDemo/service/MyUserDetailsService.java
+++ src/main/java/com/ddg/SpringSecurityDemo/service/MyUserDetailsService.java
@@ -1,13 +1,28 @@
 package com.ddg.SpringSecurityDemo.service;
 
+import com.ddg.SpringSecurityDemo.dao.UserRepo;
+import com.ddg.SpringSecurityDemo.model.User;
+import com.ddg.SpringSecurityDemo.model.UserPrincipal;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
 public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepo repo;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        User user = repo.findByUsername(username);
+
+        if (user == null) {
+            System.out.println("User not found");
+            throw new UsernameNotFoundException("User not found");
+        }
+        return new UserPrincipal(user);
     }
 }
