2c2abde49e5c78c206483d4a916ce0cc0935043d
gaddganepola <ganeypola@gmail.com>
2025-08-09 09:13:45 +0530

    get session id

--- src/main/java/com/ddg/SpringSecurityDemo/controller/HelloController.java
+++ src/main/java/com/ddg/SpringSecurityDemo/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.ddg.SpringSecurityDemo.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping("hello")
-    public String hello() {
-        return "hello";
+    public String hello(HttpServletRequest request) {
+        return "hello " + request.getSession().getId();
+    }
+
+    @GetMapping("about")
+    public String about(HttpServletRequest request) {
+        return "about " + request.getSession().getId();
     }
 }
