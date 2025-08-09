96013df2dd230e2c1a69239f035b9a9bdf1dca25
gaddganepola <ganeypola@gmail.com>
2025-08-09 17:19:50 +0530

    disable csrf

--- src/main/java/com/ddg/SpringSecurityDemo/config/SecurityConfig.java
+++ src/main/java/com/ddg/SpringSecurityDemo/config/SecurityConfig.java
@@ -3,8 +3,10 @@ package com.ddg.SpringSecurityDemo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,6 +15,16 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        Disable CSRF
+        http.csrf(customizer -> customizer.disable());
+//        Enable security for all requests
+        http.authorizeHttpRequests(request -> request.anyRequest().authenticated());
+//        Enable the form login
+        http.formLogin(Customizer.withDefaults());
+//        Enable basic security
+        http.httpBasic(Customizer.withDefaults());
+//        Make the requests stateless
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return http.build();
     }
 }
